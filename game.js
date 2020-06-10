27715d6ff46631ca68d1bc6823af1142d015a034
Zahra Azin <ztara.azin@gmail.com>
2020-06-10 19:58:49 -0400

    update

--- game.js
+++ game.js
@@ -26,72 +26,30 @@ fetch("questions.json")
       console.error(err);
   });
 
-  //time
+//time
 
-//set minutes 
- var mins = 2; 
-  
-//calculate the seconds 
-var secs = mins * 60; 
-    
-//countdown function is evoked when page is loaded 
-function countdown() { 
-    setTimeout('Decrement()', 60); 
-} 
-    
-//Decrement function decrement the value. 
-function Decrement() { 
-    if (document.getElementById) { 
-        minutes = document.getElementById("minutes"); 
-        seconds = document.getElementById("seconds"); 
-    
-        //if less than a minute remaining 
-        //Display only seconds value. 
-        if (seconds < 59) { 
-        seconds.value = secs; 
-        } 
-    
-        //Display both minutes and seconds 
-        //getminutes and getseconds is used to 
-        //get minutes and seconds 
-        else { 
-            minutes.value = getminutes(); 
-            seconds.value = getseconds(); 
-        } 
-        //when less than a minute remaining 
-        //colour of the minutes and seconds 
-        //changes to red 
-        if (mins < 1) { 
-            minutes.style.color = "red"; 
-            seconds.style.color = "red"; 
-        } 
-        //if seconds becomes zero, 
-        //then page alert time up 
-        if (mins < 0) { 
-            alert('time up'); 
-            minutes.value = 0; 
-            seconds.value = 0; 
-        } 
-        //if seconds > 0 then seconds is decremented 
-        else { 
-            secs--; 
-            setTimeout('Decrement()', 1000); 
-        } 
-    } 
-} 
-    
-function getminutes() { 
-    //minutes is seconds divided by 60, rounded down 
-    mins = Math.floor(secs / 60); 
-    return mins; 
-} 
-    
-function getseconds() { 
-    //take minutes remaining (as seconds) away  
-    //from total seconds remaining 
-    return secs - Math.round(mins * 60); 
+function startTimer(duration, display) {
+  var timer = duration, minutes, seconds;
+  setInterval(function () {
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
+
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      display.textContent = minutes + ":" + seconds;
+
+      if (--timer < 0) {
+          timer = duration;
+      }
+  }, 1000);
 }
 
+window.onload = function () {
+  var fiveMinutes = 60 * 5,
+      display = document.querySelector('#time');
+  startTimer(fiveMinutes, display);
+};
 
 //CONSTANTS
 const CORRECT_BONUS = 10;
@@ -117,7 +75,6 @@ getNewQuestion = () => {
   }
   questionCounter++;
   progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
-  //Update the progress bar
   progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
   const questionIndex = Math.floor(Math.random() * availableQuesions.length);
